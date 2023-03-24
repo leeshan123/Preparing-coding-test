bffb227be7249f71686ff89d69164e412d7f99c6
coz93 <coz93@naver.com>
2023-03-24 14:49:19 +0900

    Update level0_022.java

--- CodingTest/level0_022.java
+++ CodingTest/level0_022.java
@@ -28,15 +28,22 @@ public class level0_022 {
 		for(int i =0; i < num_list.length; i++) {
 			answer[i] = num_list[num_list.length -i-1];
 		}
+		// other for 문 은 위 for문의 -i-1 '-1' 이라는 값 소거가능.
+	 	// for ( int i = num_list.length ; i >= 0 ; i-- ) {
+		//	answer[num_list.length-i] = num_list[i];
+		// }
+		// other for 문
+		
 		// 여기서num_list.length -i에 -1을 해줘야함. 배열은 0 부터 시작하기 때문에
-		for(int i =0; i < num_list.length; i++)
+		for(int i =0; i < num_list.length; i++)       //  <-- 위에 for문은 for문 안에 로직이 1줄이여도 { } 를 감싸줬는데 왜 여기에선 안 감쌋는지?
 			System.out.println(answer[i]);
 		
 		        return answer;
-		        //리턴 값때문에 주소값이 나옴
+		        //리턴 값때문에 주소값이 나옴         // <-- 리턴 값 '떄문에'가 아니라 리턴 값을 배열자체로 하니깐 배열의 주소가 print 되는것임
 		        
 		    }
 		}
-
+		
+	}
 
 
